575f39246c0973f3d2799f5cb28481c15c555486
Zuzana Stavjaňová <zuzana.stavjanova@carvago.com>
2021-05-16 11:59:23 +0200

    Remove one curly brackets to fix css

--- src/components/app/alert/Alert.js
+++ src/components/app/alert/Alert.js
@@ -2,23 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledAlert = styled.div`
-height: 8%;
-width: 100%;
-position: absolute;
-background-color: #00FF41;
-display: flex;
-justify-content: center;
-align-items: center;
-// display: none;
-   & > p {
+  height: 8%;
+  width: 100%;
+  position: absolute;
+  background-color: #00ff41;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // display: none;
+  & > p {
     color: #000;
     font-weight: 800;
     font-size: 30px;
     line-height: 130%;
     letter-spacing: 0.03em;
     text-transform: uppercase;
-   }
-}
+  }
 `;
 
 export const Alert = (props) => {
